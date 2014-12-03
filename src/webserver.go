bb164c36a8b1e726a6feb1fcabf7ea241a3da4c0
Brian Lamb <brianlambtrumpet@gmail.com>
2014-12-02 20:59:09 -0700

    Finished post logic. Can now successfully call and wait for external program to finish

--- src/webserver.go
+++ src/webserver.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 func hello(output http.ResponseWriter, request *http.Request) {
@@ -14,7 +15,7 @@ func hello(output http.ResponseWriter, request *http.Request) {
 	}
 	file, error := os.Open("html"+request.URL.Path)
 	if error != nil && error !=io.EOF {
-		
+
 		panic(error)
 	}
 	if(request.URL.Path[len(request.URL.Path)-3:] == "css"){
@@ -22,7 +23,7 @@ func hello(output http.ResponseWriter, request *http.Request) {
 		header.Set("Content-Type", "text/css")
 	}
 	buffer := make([]byte, 2048)
-	
+
 	for {
 		n,error := file.Read(buffer)
 		if error != nil && error !=io.EOF {
@@ -37,8 +38,18 @@ func hello(output http.ResponseWriter, request *http.Request) {
 
 func submit(output http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
-	fmt.Println(request.PostForm)
-	http.Redirect(output, request, "/quotanizer/index.html", 1);
+
+	fmt.Printf("FilePath: %s\nMinimum File Size: %s\n", request.PostForm["dir"][0], request.PostForm["minSize"][0])
+
+	//Run external program and wait for it to finish
+	cmd := exec.Command("sleep", "5")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Send browser the the generated page
+	http.Redirect(output, request, "/index.html", 302)
 }
 
 func main() {
